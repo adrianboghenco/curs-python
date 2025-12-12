c8288ff65884b05adc57935e8df62622467d4aec
adrianboghenco <boghenco.hadrian@gmail.com>
2025-12-12 16:49:50 +0000

    Update variabile.py

--- variabile.py
+++ variabile.py
@@ -7,9 +7,13 @@ variabala_2 = 'text'
 int variabila # este o variabla ce memoreaza doar numere intregi
 
 # variabilele nu pot incepe cu un numar, pot incepe cu _, nu pot avea un spatiu
-
+# de dorit ca variabilele sa fie cu litere mici, un fel de conventie 
+# nu pot incepe cu def, import, if, for s.a
 ''' sfasf
 exemplu de comentarii usate cu trei linii sus
 '''
 
 variabila_mea = 1
+
+variabila_mea = "text" 
+
