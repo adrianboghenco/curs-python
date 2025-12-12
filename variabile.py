35897c1064caf6be0a5a65eadd7cdcf6c536f4a0
adrianboghenco <boghenco.hadrian@gmail.com>
2025-12-12 17:32:38 +0000

    Update variabile.py

--- variabile.py
+++ variabile.py
@@ -18,4 +18,47 @@ variabila_mea = 1
 variabila_mea = "text" 
 
 variabila_2 = 65
+nume_familie = "Adrian Boghenco" # de dat o denumire sugestiva ca sa ne dam seama ce tip de variabila este 
+
+a = 10 
+nota_maxima = 10 
+
+# variabila de tip string, cu apostrof sau ghilimele
+
+variabila_string = 'Ana are mere'
+variabila_string = "Ana are mere"
+variabila_string = '' # o variabila care contine un string gol 
+# sau 
+variabila_string = 'a' # contine un string format dintr-o litera
+
+variabala_int = 10
+variabila_float = 2.5 
+
+var_str = '''Ana are 
+multe 
+mere
+'''
+# se pot folosi si cite 3 ghilimele """ """
+# per general se folosteste 'apostrof'
+var_str = "don't" 
+var_str = 'citat din Citat "Emil Cioran"'
+
+var_int = -10 # numar intreg negativ
+var_foat = -0.99 # numar negativ de tip float, cu virgula
+
+# Boolean, este ca raspuns: True sau False
+var_bool = True
+var_bool = False 
+
+# none Type, absenta unei valopri None
+var_meah = None # exista valoarea dar nu stocheaza, se folosesc pentru functii, sau pentru a verifica o anumita
+# conditie 
+cod1
+cod2
+cod3
+
+verificare 
+cod3 ? ok
+var_meah = True 
+
 
